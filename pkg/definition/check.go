fd3e51c4209bef434107b885648bfac39c025ec6
dr-dobermann <rgabitov@gmail.com>
2025-09-29 05:56:10 +0500

    Fix lexical error in comments

--- pkg/definition/check.go
+++ pkg/definition/check.go
@@ -7,36 +7,36 @@ import (
 	"github.com/dr-dobermann/gonfa/pkg/gonfa"
 )
 
-// checkStates checks finalState presense in states and if they
-// are reachible.
+// checkStates checks final state presence in states and if they
+// are reachable.
 func checkStates(
 	initialState gonfa.State,
 	states []gonfa.State,
 	transitions []Transition,
 	finalStates []gonfa.State,
 ) error {
-	// check if initial state is in states
+	// Check if initial state is in states
 	if !slices.Contains(states, initialState) {
-		return fmt.Errorf("initial state '%s' isn't exists in states", initialState)
+		return fmt.Errorf("initial state '%s' doesn't exist in states", initialState)
 	}
 
-	// check if all final states are presented in states
+	// Check if all final states are present in states
 	for _, fs := range finalStates {
 		if !slices.Contains(states, fs) {
 			return fmt.Errorf("final state '%s' isn't found in states", fs)
 		}
 	}
 
-	// build state graph for states and transitions analyzis
+	// Build state graph for states and transitions analysis
 	tss := map[gonfa.State][]gonfa.State{initialState: {}}
 	for _, t := range transitions {
 		if !slices.Contains(states, t.From) {
-			return fmt.Errorf("state '%s' isn't existed as transition source",
+			return fmt.Errorf("state '%s' doesn't exist as transition source",
 				t.From)
 		}
 
 		if !slices.Contains(states, t.To) {
-			return fmt.Errorf("state '%s' isn't existed as transition target", t.To)
+			return fmt.Errorf("state '%s' doesn't exist as transition target", t.To)
 		}
 
 		tt, found := tss[t.From]
@@ -60,7 +60,7 @@ func checkStates(
 
 // transCounter holds number of input and output transitions from the state.
 type transCounter struct {
-	// traversed indicates that this state tracked or not in previous passes
+	// traversed indicates whether this state was tracked in previous passes
 	traversed bool
 	in        int
 	out       int
@@ -73,60 +73,60 @@ func analyzeGraph(
 	states []gonfa.State,
 	tss map[gonfa.State][]gonfa.State,
 ) error {
-	// check if initial state is really used as initial state
+	// Check if initial state is really used as initial state
 	if _, found := tss[initialState]; !found {
-		return fmt.Errorf("no transitions starts from initial state '%s'", initialState)
+		return fmt.Errorf("no transitions start from initial state '%s'", initialState)
 	}
 
 	stc := make(map[gonfa.State]transCounter, len(states))
 
-	// trace state transitions from initial state
+	// Trace state transitions from initial state
 	us := []gonfa.State{initialState}
 	stc[initialState] = transCounter{}
 	for len(us) > 0 {
 		s := us[0]
 
-		// add unchecked states from traverse
+		// Add unchecked states from traverse
 		us = append(us, traverse(s, tss, stc)...)
 
-		// remove s as checked
+		// Remove s as checked
 		us = us[1:]
 	}
 
-	// to check if the final state reachible,
-	// all the final states are marked as unreacible and put to urfs.
-	// if state has no outcoming transitions and it is presneted in final states list
-	// the state removes from urfs.
-	// if urfs isn't empty after check then the definition is broken since it has
-	// unreachible final states.
+	// To check if the final state is reachable,
+	// all the final states are marked as unreachable and put to urfs.
+	// If state has no outgoing transitions and it is presented in final states list
+	// the state is removed from urfs.
+	// If urfs isn't empty after check then the definition is broken since it has
+	// unreachable final states.
 	urfs := make([]gonfa.State, len(finalStates))
 	n := copy(urfs, finalStates)
 	if n != len(finalStates) {
-		panic("copying finalStates to unreachible final states list failed")
+		panic("copying finalStates to unreachable final states list failed")
 	}
 
 	for s, sc := range stc {
-		// check for hanging states -- the states, which has no incoming transitions but
-		// isn't initialState.
+		// Check for hanging states -- the states, which have no incoming transitions but
+		// aren't initialState.
 		if sc.in == 0 && s != initialState {
 			return fmt.Errorf(
-				"state '%s' isn't an initial state but not has incoming transitions", s)
+				"state '%s' isn't an initial state but has no incoming transitions", s)
 		}
 
 		isFinal := slices.Contains(finalStates, s)
 
-		// check for deadend states -- the states, which has no outgoing transitions but
-		// isn't one of the final state.
+		// Check for deadend states -- the states, which have no outgoing transitions but
+		// aren't one of the final states.
 		if isFinal && sc.out == 0 {
 			return fmt.Errorf("state '%s' is a deadend state", s)
 		}
 
-		// check for invalid final states (has outgoing transitions)
+		// Check for invalid final states (has outgoing transitions)
 		if isFinal && sc.out > 0 {
 			return fmt.Errorf("final state '%s' has outgoing transition(s)", s)
 		}
 
-		// delete final state from the unreachible final states list if it has incoming
+		// Delete final state from the unreachable final states list if it has incoming
 		// transition.
 		if isFinal && sc.in > 0 {
 			urfs = slices.DeleteFunc(
@@ -138,7 +138,7 @@ func analyzeGraph(
 	}
 
 	if len(urfs) > 0 {
-		return fmt.Errorf("definitin has unreachible final state(s): %v", urfs)
+		return fmt.Errorf("definition has unreachable final state(s): %v", urfs)
 	}
 
 	return nil
@@ -147,7 +147,7 @@ func analyzeGraph(
 // traverse traces single transition path from state s to the end state and
 // returns all possible state's forks on the way.
 // traverse checks the repeatable passes of the states and updates input and
-// ouptut transitions of the state.
+// output transitions of the state.
 func traverse(
 	s gonfa.State,
 	tss map[gonfa.State][]gonfa.State,
@@ -165,7 +165,7 @@ func traverse(
 	for _, ts := range tss[s] {
 		si.out += 1
 
-		// find target state in state counters map
+		// Find target state in state counters map
 		tsc, found := stc[ts]
 		if !found {
 			tsc = transCounter{}
